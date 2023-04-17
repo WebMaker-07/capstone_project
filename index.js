2336dd0aa3232d03316bcb4be39d650b808784ad
profferlhastlyfaner07@gmail.com <profferlhastlyfaner07@gmail.com>
2023-04-17 05:46:20 -0700

    adding hbs and designing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require('express'); // calling the express module
 const path = require('path');// calling the path module
 const app = express(); // passing the express module to app variable
-const port = 5500;// declare the port for the app
+const port = 5000;// declare the port for the app
 const env = require('dotenv');// calling the dotenv
 const mysql = require('mysql2');// calling the mysqls
 
@@ -11,12 +11,13 @@ env.config({
 
 app.set('view engine', 'hbs');
 // priority to call first
+
 app.use(express.urlencoded(
     {
         extended: true
     }));
 
-
+ app.use(express.static('public'));
 //define the routes
 app.use('/', require('./routes/admin_routes'));
 // app.use('/auth',require('./routes/auth'));
--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -11,4 +11,20 @@ router.get('/register_admin',(request,response)=>{
     response.render('register_admin');
 })
 
+// routes in admin dashboards
+
+router.get('/dashboard',(request,response)=>{
+    response.render('admin/home');
+})
+router.get('/customers',(request,response)=>{
+    response.render('admin/customers');
+})
+router.get('/product-list',(request,response)=>{
+    response.render('admin/product_list');
+})
+router.get('/product-categories',(request,response)=>{
+    response.render('admin/product_category');
+})
+
+
 module.exports = router;

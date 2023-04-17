97be1f48cff06b344df1e5b925d6d8fb3412145d
patrickdelumen <delumenpatrick@gmail.com>
2023-04-17 23:05:23 +0800

    add product_category

---
+++ controller/auth_product_category.js
@@ -0,0 +1,9 @@
+// Assuming you are using Express.js and the mysql2 library
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql2');
+
+
+
+// Export the router
+module.exports = router;

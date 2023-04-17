e40651445b3b06e7a53082c4d32462936d6c4518
mikefillar <mikefillar21@gmail.com>
2023-04-17 23:00:43 +0800

    changes in other files

--- controller/auth_account.js
+++ controller/auth_account.js
@@ -1,9 +1,26 @@
-// const mysql = require('mysql2');
-// const db = mysql.createConnection(
-//     {
-//        host:  process.env.DATABASE_HOST,
-//         user: process.env.DATABASE_USER,
-//         password: process.env.DATABASE_PASSWORD,
-//         database: process.env.DATABASE,
-//         port:process.env.DATABASE_PORT
-//     });
+const mysql = require('mysql2');
+const db = mysql.createConnection(
+    {
+        host:  process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE,
+        port:process.env.DATABASE_PORT
+    });
+
+exports.viewCustomer = (req,res)=>{
+    console.log('HELLO!');
+    res.send('Hello');
+    // db.query('SELECT * from customers',
+    //     (error,result)=>
+    //         {
+    //             if(error)
+    //                 {
+    //                     console.log("Error Message : " + error);
+    //                 }
+    //             else if(result)
+    //                 {
+    //                     console.log(result);
+    //                 }
+    //         });
+}
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express(); // passing the express module to app variable
 const port = 5000;// declare the port for the app
 const env = require('dotenv');// calling the dotenv
 const mysql = require('mysql2');// calling the mysqls
-
 env.config({
     path: './.env'
 })// config the path of env
@@ -17,31 +16,31 @@ app.use(express.urlencoded(
         extended: true
     }));
 
- app.use(express.static('public'));
+app.use(express.static('public'));
 //define the routes
 app.use('/', require('./routes/admin_routes'));
-// app.use('/auth',require('./routes/auth'));
+app.use('/auth',require('./routes/auth'));
 //creating a connection in database
 app.use(express.json())
-const mydb = mysql.createConnection(
-    {
-        host: process.env.DATABASE_HOST,
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE,
-        port: process.env.DATABSE_PORT
-    }
-);
+// const mydb = mysql.createConnection(
+//     {
+//         host: process.env.DATABASE_HOST,
+//         user: process.env.DATABASE_USER,
+//         password: process.env.DATABASE_PASSWORD,
+//         database: process.env.DATABASE,
+//         port: process.env.DATABSE_PORT
+//     }
+// );
 app.listen(port,()=>{
     console.log(`Server has started ${port}`);// testing the port
     //Condition to test if succesfully connected in databse
-    mydb.connect((error)=>{
-        if (error){
-            console.log('Error occured:' + error);
-        }else{
-            console.log('Database Connected Sucessfully')
-        }
+    // mydb.connect((error)=>{
+    //     if (error){
+    //         console.log('Error occured:' + error);
+    //     }else{
+    //         console.log('Database Connected Sucessfully')
+    //     }
         
-    })
+    // })
 
 })

f06205e3d362f22042b0d589045d6a12b3d1ab1d
patrickdelumen <delumenpatrick@gmail.com>
2023-04-18 16:43:11 +0800

    patz

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const admin_controller = require('../controller/auth_account');
 
-router.post('/customers',admin_controller.viewCustomer);
+router.post('/customers', (req, res) => {
+  console.log('viewCustomer is called');
+  admin_controller.viewCustomer(req, res);
+});
 
 module.exports = router;

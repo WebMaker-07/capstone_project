24cd8e08832267eb11ddf8b76934eec713ff6b04
mikefillar <mikefillar21@gmail.com>
2023-04-18 16:38:47 +0800

    customer interface updated

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const admin_controller = require('../controller/auth_account');
 
-router.post('/customers',admin_controller.viewCustomer);
+router.get('/customers',admin_controller.viewCustomer);
 
 module.exports = router;

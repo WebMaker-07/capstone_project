0a6a6512d1224ac27730c5d9dacc8be05cc05ee1
patrickdelumen <delumenpatrick@gmail.com>
2023-05-01 13:09:46 +0800

    add product list wala pang update at add . meron nang view at delete

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const product_cat = require('../controller/auth_product_category');
 const admin_controller = require('../controller/auth_account');
+const product_list = require('../controller/auth_product_list');
+
 
 //CUSTOMERS
 router.get('/customers',admin_controller.view_customer);
@@ -17,5 +19,9 @@ router.get('/deleteProductCat/:category_id', product_cat.deleteProductCat);
 router.get('/auth/updateProductCat/:category_id', product_cat.updateProductCat);
 router.post('/auth/updateProductCat/:category_id', product_cat.updateProductCatSubmit);
 
+//PRODUCT LIST
+router.get('/product-list',product_list.viewProductList);
+router.post('/product_add',product_list.addProductList);
+router.get('/deleteProduct/:product_id',product_list.deleteProductList);
 
 module.exports = router;

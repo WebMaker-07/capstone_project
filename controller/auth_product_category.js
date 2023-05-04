92639e9f438c129ddf5ce350bdf16fc021379b6e
patrickdelumen <delumenpatrick@gmail.com>
2023-05-04 10:28:19 +0800

    add some function in product category

--- controller/auth_product_category.js
+++ controller/auth_product_category.js
@@ -29,45 +29,65 @@ const db = mysql.createConnection(
             
         });
     }
-
-    exports.addProductcat = (req,res) =>{
-        let {category_name} = req.body;
-        
-        if (!category_name) { 
-            res.render('admin/product_category', {
-                title: "List of category",
-                message: "Please enter a category name!",
-                color: "alert-danger"
-            });
-            return; 
-        }
-        
-        
-        db.query('INSERT INTO products_category(category_name) VALUES(?)', 
-            category_name
-        , (err, result) => {
-            if (err) {
-                console.log('Error Message: ' + err);
-            } else {
-                db.query('SELECT * FROM products_category', (err, data) => {
-                    if (err) {
-                        console.log('Error Message: ' + err);
-                    } else {
-                        res.render('admin/product_category', {
-                            title: "List of category",
-                            data: data,
-                            message: "Product Category added successfully!",
-                            color: "alert-success"
-                        })
-                        
-                    }
-                })
-            }
-        })
-
+        // update category
+    exports.updateCategory = (req, res) => {
+      const { category_id, category_name } = req.body;
+      db.query(
+          'UPDATE products_category SET category_name = ? WHERE category_id = ?',
+          [category_name, category_id],
+          (err, result) => {
+              if (err) {
+                  console.log(err);
+                  res.status(500).send('An error occurred while updating the category.');
+              } else {
+                  console.log(result);
+                  res.redirect('/admin/product_category');
+              }
+          }
+      );
     }
-    
 
+    // add or update category
+    exports.addProductcat = (req,res)=>{
+      const { category_name } = req.body;
+      db.query('INSERT INTO products_category SET ?',
+      {
+          category_name: category_name,
+          store_id: 1 // add default value for store_id
+      },
+      (err,result)=>
+      {
+              // console.log(db);
+              if(err)
+                  {
+                      console.log('Error Message: '+err);
+                  }
+              else
+                  {
+                      db.query('SELECT * FROM products_category',(err,data)=>
+                          {
+                              output= data[0];
+                              if(err)
+                                  {
+                                      console.log('Error Message: '+err);
+                                  }
+                              else
+                                  {
+                                      res.render('admin/product_category',
+                                          {
+                                              data: output,
+                                              message: "product added successfully!",
+                                              color: "alert-success"
+                                          });
+                                  }
+                          });
+                  }
+          });
+    };
+        
+
+    
+    
     exports.deleteProductCat = (req, res) => {
         const categoryId = req.params.category_id;
         db.query("DELETE FROM products_category WHERE category_id = ?", [categoryId], 
@@ -91,44 +111,9 @@ const db = mysql.createConnection(
         });
       };
       
-      exports.updateProductCat = (req, res) => {
-        const categoryId = req.params.category_id;
-        db.query("SELECT * FROM products_category WHERE category_id = ?", [categoryId], (err, data) => {
-          if (err) {
-            console.log('Error Message: ' + err);
-          } else {
-            res.render('admin/product_category', {
-              title: "Update Product Category",
-              data: data[0]
-            });
-          }
-        });
-      };
 
 
       
-      exports.updateProductCatSubmit = (req, res) => {
-        const categoryId = req.params.category_id;
-        const { category_name } = req.body;
-        db.query("UPDATE products_category SET category_name = ? WHERE category_id = ?", [category_name, categoryId], (err, result) => {
-          if (err) {
-            console.log('Error Message: ' + err);
-          } else {
-            db.query('SELECT * FROM products_category', (err, data) => {
-              if (err) {
-                console.log('Error Message: ' + err);
-              } else {
-                res.render('admin/product_category', {
-                  title: "List of Product Category",
-                  data: data,
-                  message: "Product Category updated successfully!",
-                  color: "alert-primary"
-                });
-              }
-            });
-          }
-        });
-      };
       
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -10,16 +10,13 @@ const product = require('../controller/auth_product');
 //CUSTOMERS
 router.get('/customers',admin_controller.view_customer);
 router.post('/add_customer',admin_controller.add_customer);
-router.get('/customer_update/:customer_id', admin_controller.update_form);
-router.post('/update_customer', admin_controller.update_customer);
 router.get('/customer_delete/:customer_id', admin_controller.customer_delete);
 
 //PRODUCT CATEGORY
 router.get('/product-categories',product_cat.viewProductCat);
-router.post('/add_category',product_cat.addProductcat);
+router.post('/product-categories',product_cat.addProductcat);
 router.get('/deleteProductCat/:category_id', product_cat.deleteProductCat);
-router.get('/auth/updateProductCat/:category_id', product_cat.updateProductCat);
-router.post('/auth/updateProductCat/:category_id', product_cat.updateProductCatSubmit);
+router.get('/auth/updatecategory/:category_id', product_cat.updateCategory);
 
 //PRODUCT LIST
 router.get('/product-list',product_list.viewProductList);

a26ece14ad519bc5805dbd255177b07072d31ada
mikefillar <mikefillar21@gmail.com>
2023-05-08 11:23:21 +0800

    new file

---
+++ controller/dashboard.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+const db = mysql.createConnection(
+    {
+        host:  process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE,
+        port:process.env.DATABASE_PORT
+    });
+
+    exports.viewDashboard = (req,res)=> {
+        
+    }
